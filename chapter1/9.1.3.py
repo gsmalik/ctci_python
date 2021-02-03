60faa637bc19ea8e3c8263de2b839162e04c208f
Gurshaant Malik <garymalik8080@gmail.com>
2021-02-03 16:36:56 -0500

    Add 9.1.4.py

--- chapter1/9.1.3.py
+++ chapter1/9.1.3.py
@@ -1,39 +1,56 @@
-class myStack():
-    def __init__(self, string):
-        self.string = []
-        for character in string:
-            self.string.append(character)
+def urlify(string, len_string):
+    """
+    Function to replace space character with '%20'.
 
-    def empty(self):
-        return len(self.string) == 0
+    Parameters
+    ----------
+    string: ``list``
+        List of string that needs to be converted to url, with appropriate space at end.
 
-    def getTopChar(self):
-        return self.string.pop(0)
+    len_string: ``int``
+        Length of string, without accounting for whitespaces at end for url.
 
-class myString():
-    def __init__(self):
-        self.string = []
+    Returns
+    -------
+    url version of string.
 
-    def appendSpecial(self):
-        self.string.append('%')
-        self.string.append('2')
-        self.string.append('0')
+    Time Complexity
+    ---------------
+    O(N)
 
-    def appendChar(self, character):
-        self.string.append(character)
+    Space Complexity
+    ----------------
+    O(1)
+    """
 
-def URLify(string):
-    myStringStack = myStack(string)
-    myNewString = myString()
-    while not myStringStack.empty():
-        character = myStringStack.getTopChar()
-        print(character)
-        if character == " ":
-            myNewString.appendSpecial()
+    def _list_to_string(l):
+        """
+        Convert a list to a string.
+        """
+        s = ""
+        for char in l:
+            s += char
+        return s
+
+    # Create an index at the every end of the complete url list.
+    replace_index = len(string) - 1
+
+    # Start iterating from the index at which the non-url list ends.
+    for char_index in reversed(range(len_string)):
+        # If white space, introduce "%20" backwards, starting from replace index.
+        if string[char_index] == " ":
+            string[replace_index] = "0"
+            string[replace_index - 1] = "2"
+            string[replace_index - 2] = "%"
+            replace_index -= 3
+        # Otherwise, just move the non-space character to the replace index.
         else:
-            myNewString.appendChar(character)
-
-    print(myNewString.string)
-
-URLify("hello ")
+            string[replace_index] = string[char_index]
+            replace_index -= 1
     
+    # Return the updates list as a string.
+    return _list_to_string(string)
+
+
+test = "mr john smith"
+print(urlify(list(test + " " * test.count(" ") * 2), len(test)))
--- chapter1/9.1.4.py
+++ chapter1/9.1.4.py
@@ -1,9 +1,36 @@
-def palindromePermutation(string):
+def palindrome_permutation(string):
+    """
+    Function to check if string is permutation of palindrome.
+
+    Parameters
+    ----------
+    string1: ``str``
+        String that needs to be checked for permuted palindrome.
+
+    Returns
+    -------
+    True if string is permuation. False otherwise.
+
+    Time Complexity
+    ---------------
+    O(N). ASCII list takes constant time to loop. Hence, ignored.
+
+    Space Complexity
+    ----------------
+    O(1). ASCII length is constant. Hence, ignored.
+    """
+
+    # Create an ascii character count.
     asciiList = [0]*128
-    print(len(asciiList))
+
+    # Iterate through each character in string and update respective count.
+    # Ignore space character (" ").
     for character in string:
         print(character, ord(character))
-        asciiList[ord(character)] += 1
+        asciiList[ord(character)] += 1 if character != " " else 0
+
+    # For a string to be a combination of palindrome, each character
+    # should occur twice except one, which would be middle of palindrom.
     oddOccurences = 0
     for count in asciiList:
         if count % 2 != 0:
@@ -13,4 +40,4 @@ def palindromePermutation(string):
     else:
         return True
 
-print(palindromePermutation("tactc a"))
+print(palindrome_permutation("tactrc a"))
