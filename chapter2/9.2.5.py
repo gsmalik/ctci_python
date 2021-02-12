ee02688c4686570711fe45ab15fdfe999d7038a8
Gurshaant Malik <garymalik8080@gmail.com>
2021-02-12 08:23:14 -0500

    Add 9.2.5.py

--- chapter2/9.2.5.py
+++ chapter2/9.2.5.py
@@ -1,33 +1,85 @@
 import numpy as np
 
-# np.random.seed(0)
+import linked_list as ll
 
-import linkledList as ll
 
-test1 = np.random.randint(0,10,5)
-test2 = np.random.randint(0,10,7)
+def create_number_from_linked_list(head):
+    """
+    Function to create a number out of given linked list.
 
-myLinkedList1 = ll.linkedList()
-myLinkedList1.createLinkedList(test1)
-myLinkedList1.traverseLinkedList()
-number1 = myLinkedList1.createNumberFromList()
+    Parameters
+    ----------
+    head: ``Node``
+        Head node of linked list.
 
-myLinkedList2 = ll.linkedList()
-myLinkedList2.createLinkedList(test2)
-myLinkedList2.traverseLinkedList()
-number2 = myLinkedList2.createNumberFromList()
 
-sum_number = number1 + number2
-print(sum_number)
+    Returns
+    -------
+    Number of type ``int``.
 
-multiplier = 10
+    Time Complexity
+    ---------------
+    O(N)
+    Space Complexity
+    ----------------
+    O(1). Number can be represented using constant space in FP64.
+    """
+    dec_position = 1
+    number = 0
+    while head:
+        number = number + dec_position * head.value
+        head = head.next
+        dec_position *= 10
+    return number
 
-myList = []
-multiplier = 10
-while sum_number != 0:
-    myList.append(sum_number % multiplier)
-    sum_number = int(sum_number/multiplier)
 
-newLinkedList = ll.linkedList()
-newLinkedList.createLinkedList(np.array(myList))
-newLinkedList.traverseLinkedList()
+def create_linked_list_from_number(number):
+    """
+    Function to create a linked list out of given number.
+
+    Parameters
+    ----------
+    number: ``int``
+        Number of which linked list will be created.
+
+
+    Returns
+    -------
+    Head of type ``node`` of resultant linked list.
+
+    Time Complexity
+    ---------------
+    O(D), where D is the number of digits in the number.
+    Space Complexity
+    ----------------
+    O(D)
+    """
+    head = None
+
+    while number != 0:
+        if not head:
+            head = ll.Node(number % 10)
+            pointer = head
+        else:
+            pointer.next = ll.Node(number % 10)
+            pointer = pointer.next
+        number = number // 10
+    return head
+
+
+# Generate 2 random linked lists
+test_1 = np.random.randint(0, 10, 5)
+test_2 = np.random.randint(0, 10, 7)
+
+head_1 = ll.create_linked_list(test_1)
+head_2 = ll.create_linked_list(test_2)
+
+# Create numbers out of linked lists
+number_1 = create_number_from_linked_list(head_1)
+print(number_1)
+number_2 = create_number_from_linked_list(head_2)
+print(number_2)
+
+# Create linked list out of summed numbers.
+new_head = create_linked_list_from_number(number_1 + number_2)
+ll.traverse_linked_list(new_head)
