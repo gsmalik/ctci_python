16239d714a50af54cc8086c7b2e0aa06379132da
Gurshaant Malik <garymalik8080@gmail.com>
2021-02-25 08:32:35 -0500

    Add 9.3.3.py

--- chapter3/9.3.3.py
+++ chapter3/9.3.3.py
@@ -1,37 +1,157 @@
-class myStack:
-    def __init__(self, limit):
-        self.userStack = [[]]
-        self.limit = limit
-        self.index = 0
+class PlateStacks:
+    """
+    A class that implements K stacks of certain maximum depth and exposes an API
+    that funtions as a single stack for the end user.
 
-    def push(self, element):
-        print(self.userStack[self.index] == [], len(self.userStack[self.index]))
-        if not self.userStack[self.index]:
-            if len(self.userStack[self.index]) < self.limit:
-                self.userStack[self.index].append(element)
-            else:
-                self.index += 1
-                self.push(element)
+    Parameters
+    ----------
+    k: ``int``
+       Number of stacks to be implemented.
 
-        else:
-            self.userStack[self.index].append(element)
+    depth : ``int``
+        Maximum depth of each stack
+
+
+    Space Complexity
+    ----------------
+    O(K).
+    """
+
+    def __init__(self, k, depth):
+        self.k = k
+        self.depth = depth
+
+        # Create a list of stacks, akin to towers of plates in a restaurant.
+        self.plate_stack = [[] for _ in range(k)]
+
+        # This array of length=k stores wether this stack is already in q. If so,
+        # it is not added to the q.
+        self.stack_in_q = [True] * k
+
+        # We maintain a queue of which stacks to use. The stack index at the head
+        # of the queue is the one used for pushing as well as popping from the
+        # unified stack. When popping from another indexed stack, we use
+        # ``stack_in_q`` to determine wether to push the index of the popped
+        # stack into the queue or not.
+        self.q = list(range(k))
+
+        # This array keeps a track of src stack which spilled causing the cuurent
+        # stack to be used.
+        self.spill_src = [-1] * k
+
+        self.occupied = 0
+
+    def push(self, val):
+        """
+        Function to push an element to a stack.
+
+        Parameters
+        ----------
+        val : ``FP32``
+            The value of element to be pushed
+
+
+        Time Complexity
+        ---------------
+        O(1)
+        """
+        # Check if space available to push.
+        if self.full():
+            print(f"Cannot push to stack. Maximum capacity reached")
+            return
+        self.occupied += 1
+
+        stack_to_use = self.q[0]
+
+        # If current stack is full, pop it from q and use the new head of the q
+        if len(self.plate_stack[stack_to_use]) == depth:
+            self.spill_src[self.q[1]] = stack_to_use
+            self.stack_in_q[stack_to_use] = False
+            self.q.pop(0)
+            stack_to_use = self.q[0]
+
+        self.plate_stack[stack_to_use].append(val)
+
+        print("**********")
+        print(f"Pushed value {val}")
+        print(f"Stack:{self.plate_stack}")
+        print(f"Queue:{self.q}")
+        print(f"Stack in Q:{self.stack_in_q}")
 
     def pop(self):
-        if not self.userStack[self.index] == []:  # not empty
-            return self.userStack[self.index].pop()
-        else:  # empty
-            self.index = -1
-            self.userStack.pop()
+        """
+        Function to pop most recently pushed value.
+
+        Time Complexity
+        ---------------
+        O(1)
+        """
+        if self.occupied == 0:
+            print("Cannot pop from stack. Its empty")
+        else:
+            if len(self.plate_stack[self.q[0]]) == 0:
+                return self.pop_index(self.spill_src[self.q[0]])
+            else:
+                return self.pop_index(self.q[0])
+
+    def pop_index(self, stack_num):
+        """
+        Function to pop element from top of selected stack.
+
+        Parameters
+        ----------
+        stack_num: ``int``
+            The stack to push the element to
+
+        Time Complexity
+        ---------------
+        O(1)
+        """
+        # Check if there is something to pop.
+        if len(self.plate_stack[stack_num]) == 0:
+            print(f"Pop Failed for stack #{stack_num}. Its empty.")
+            return None
+
+        self.occupied -= 1
+
+        # If the stack we are popping from does not exist in q, add it to the end
+        # of q
+        if not self.stack_in_q[stack_num]:
+            self.q.append(stack_num)
+            self.stack_in_q[stack_num] = True
+
+        popped_val = self.plate_stack[stack_num].pop()
+        print("**********")
+        print(f"Popped value {popped_val}")
+        print(f"Stack:{self.plate_stack}")
+        print(f"Queue:{self.q}")
+        print(f"Stack in Q:{self.stack_in_q}")
+
+        return popped_val
+
+    def full(self):
+        return self.occupied == self.depth * self.k
 
 
-test = myStack(1)
-test.push(2)
-test.push(3)
-test.push(4)
-test.push(5)
-print(test.pop())
-print(test.pop())
-print(test.pop())
-print(test.pop())
-test.push(6)
-print(test.pop())
+k = 3
+depth = 2
+plate_stack = PlateStacks(k, depth)
+plate_stack.push(1)
+plate_stack.push(2)
+plate_stack.push(3)
+plate_stack.pop()
+plate_stack.push(4)
+plate_stack.pop_index(0)
+plate_stack.pop()
+plate_stack.pop()
+plate_stack.pop()
+plate_stack.push(1)
+plate_stack.push(2)
+plate_stack.push(3)
+plate_stack.push(4)
+plate_stack.push(5)
+plate_stack.push(6)
+plate_stack.pop()
+plate_stack.pop_index(2)
+plate_stack.pop()
+plate_stack.pop()
