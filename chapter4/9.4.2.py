e7871d6e792636b0284435370f0316627065fb2d
Gurshaant Malik <garymalik8080@gmail.com>
2021-03-03 17:55:37 -0500

    Add 9.4.2.py

--- chapter4/9.4.2.py
+++ chapter4/9.4.2.py
@@ -1,12 +1,18 @@
-class Node():
-    def __init__(self, array):
-        middle = int(len(array)/2)
-        self.value = array[middle]
-        print(self.value)
-        if len(array) > 1:
-            self.left = Node(array[:middle])
-        if len(array) > 2:
-            self.right = Node(array[middle+1:])
+import graphs
 
+def create_bst(array):
+    middle = len(array)//2
+    root = graphs.BinaryNode(array[middle])
+    if middle < len(array) - 1:
+        root.right = create_bst(array[middle+1:])
+    if middle > 0:
+        root.left = create_bst(array[:middle])
 
-Node([1,2,3,4,5,6,7,8])
+    return root
+
+test = [1,2,3,4,5,6,7,8]
+root = create_bst(test)
+print(f"{root.value} {root.left.value} {root.right.value}")
+test = [1]
+root = create_bst(test)
+print(f"{root.value} {root.left} {root.right}")
