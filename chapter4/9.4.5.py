3f86d734d091675f53b8c7f075b2cb2ef1b8f984
Gurshaant Malik <garymalik8080@gmail.com>
2021-03-05 08:36:24 -0500

    Add 9.4.5.py

--- chapter4/9.4.5.py
+++ chapter4/9.4.5.py
@@ -1,24 +1,53 @@
 from binarytree import tree, bst
 
-def checkBSTTree(node):
-    checkBST(node)
-    if node.left:
-        checkBSTTree(node.left)
-    if node.right:
-        checkBSTTree(node.right)
 
-def checkBST(node):
-    print(node.val)
-    leftVal = node.left.val if node.left else -999999
-    rightVal = node.right.val if node.right else 999999
+def check_bst(node):
+    """
+    Function to check if binary tree is BST.
 
-    if not (leftVal <= node.val <= rightVal):
-        print("Not BST")
-        return False
-    else:
-        return True
+    Parameters
+    ----------
+    node: ``Node``
+        Root node of binary tree.
 
+    Time Complexity
+    ---------------
+    O(N), where N is the number of nodes in the binary tree. In the worst case, you may
+    have a BST, which would require you to visit each node in the tree.
+
+    Space Complexity
+    ---------------
+    O(log(N)), where N is the number of vertices in graph. We can have a stack depth of
+    recursive calls of O(log(N)) and at each depth we only use constant 6 variables to
+    keep tree of left and right subtrees' BST characteristics and values.
+    """
+    # Check wether left subtree maintained its BST properties and note value
+    # of left root node of left subtree.
+    (left_val, left_bst) = check_bst(node.left) if node.left else (None, True)
+    # Check wether right subtree maintained its BST properties and note value
+    # of right root node of right subtree.
+    (right_val, right_bst) = check_bst(node.right) if node.right else (None, True)
+
+    # Construct BST property check for this node.
+    node_greater_left = left_val is None or left_val < node.value
+    right_greater_node = right_val is None or right_val > node.value
+
+    return (
+        node.value,
+        node_greater_left and right_greater_node and left_bst and right_bst,
+    )
+
+
+# Generate binary search tree
 my_tree = bst(height=5)
-
 print(my_tree)
-checkBSTTree(my_tree.levels[0][0])
+# This should be True
+print(check_bst(my_tree.levels[0][0]))
+
+# Generate binary search tree
+my_tree = bst(height=5)
+# Intentionally make it non BST
+my_tree.levels[1][0].value = my_tree.levels[0][0].value + 1
+print(my_tree)
+# This should be False
+print(check_bst(my_tree.levels[0][0]))
