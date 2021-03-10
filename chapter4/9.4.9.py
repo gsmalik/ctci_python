ff21f40b3fede9bf80d49f9d7f46e1804d3bd649
Gurshaant Malik <gary.malik@appliedbrainresearch.com>
2021-03-10 08:38:09 -0500

    Add 9.4.9.py

--- chapter4/9.4.9.py
+++ chapter4/9.4.9.py
@@ -1,29 +1,36 @@
 from binarytree import tree, bst
 
+def find_combinations(root):
+    if root is None:
+        return [[]]
 
-def appendChildren(node, levelWiseList):
-    if node.left:
-        levelWiseList.append(node.left)
-    if node.right:
-        levelWiseList.append(node.right)
-    # print("[appendChildren] Updated list:", levelWiseList)
-    return levelWiseList
+    left_combinations = find_combinations(root.left)
+    right_combinations = find_combinations(root.right)
 
+    combinations = []
+    for l_combination in left_combinations:
+        for r_combination in right_combinations:
+            for x in mix_n_match(l_combination, r_combination):
+                combinations.append(x)
 
-def listLevelWiseNodes(rootNode):
-    levelWiseList = [[rootNode]]
-    print(levelWiseList)
-    index = 0
-    while levelWiseList[index]:
-        levelWiseList.append([])
-        for node in levelWiseList[index]:
-            # print("[listLevelWiseNodes] Current node:", node)
-            levelWiseList[index + 1] = appendChildren(node, levelWiseList[index + 1])
-        index += 1
+    if combinations:
+        combinations = [[root.value] + x for x in combinations]
+    else:
+        combinations = [[root.value]]
+    return combinations
 
-    return levelWiseList
+def mix_n_match(left, right):
+    if left and right:
+        # Make first element left
+        left_firsts = [[left[0]] + x for x in mix_n_match(left[1:], right)]
+        # Make first element right
+        right_firsts = [[right[0]]+x for x in mix_n_match(left, right[1:])]
 
+        # Add all combinations to list
+        return left_firsts +right_firsts
 
-my_tree = bst(height=5)
+    return [left] if left else [right] if right else []
+
+my_tree = bst(height=2)
 print(my_tree)
-print(listLevelWiseNodes(my_tree.levels[0][0]))
+print(find_combinations(my_tree.levels[0][0]))
