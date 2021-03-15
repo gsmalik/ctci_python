c7bdcc32b14f99f816a94ab7fee64b8efe1c8880
Gurshaant Malik <gary.malik@appliedbrainresearch.com>
2021-03-15 16:56:06 -0400

    Add comments for 9.4.2

--- chapter4/9.4.2.py
+++ chapter4/9.4.2.py
@@ -1,18 +1,13 @@
 import graphs
 
-def create_bst(array):
-    middle = len(array)//2
-    root = graphs.BinaryNode(array[middle])
-    if middle < len(array) - 1:
-        root.right = create_bst(array[middle+1:])
-    if middle > 0:
-        root.left = create_bst(array[:middle])
+# Create test array
+test = [1, 2, 3, 4, 5, 6, 7, 8]
 
-    return root
-
-test = [1,2,3,4,5,6,7,8]
-root = create_bst(test)
+#  Create BST
+root = graphs.create_bst(test, None)
 print(f"{root.value} {root.left.value} {root.right.value}")
+
+# Test a corner case of array having a single element only
 test = [1]
-root = create_bst(test)
+root = graphs.create_bst(array=test, parent_node=None)
 print(f"{root.value} {root.left} {root.right}")
