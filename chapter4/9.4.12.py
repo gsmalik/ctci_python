6c67a7475dea961e58d18a0d763414b4b7e48894
Gurshaant Malik <gary.malik@appliedbrainresearch.com>
2021-03-19 16:26:46 -0400

    Add comments to 9.4.12.py

--- chapter4/9.4.12.py
+++ chapter4/9.4.12.py
@@ -1,18 +1,22 @@
 from binarytree import tree, bst
 
 
-def depth_first_search(node, sums, hashtable):
+def depth_first_traversal(node, sums, hashtable):
+    """Just your everyday DFT"""
     sums = [x + node.val for x in sums]
     sums.append(node.val)
     hashtable = update_hashtable(sums, hashtable)
     if node.left:
-        hashtable = depth_first_search(node.left, sums, hashtable)
+        hashtable = depth_first_traversal(node.left, sums, hashtable)
     if node.right:
-        hashtable = depth_first_search(node.right, sums, hashtable)
+        hashtable = depth_first_traversal(node.right, sums, hashtable)
     return hashtable
 
 
 def update_hashtable(sums, hashtable):
+    """
+    Updates hashtable. if key exists, increments value else creates key.
+    """
     for val in sums:
         if val in hashtable:
             hashtable[val] += 1
@@ -21,8 +25,33 @@ def update_hashtable(sums, hashtable):
     return hashtable
 
 
-def count_sum_paths(rootNode):
-    return depth_first_search(rootNode, [], {})
+def count_sum_paths(root_node):
+    """
+    Function to create a hashtable where a keys in the hashtable are unique sum values
+    found in the tree and corresponding values are the count of number of times that
+    sum can be uniquely created.
+
+    Parameters
+    ----------
+    root_node: `Node`
+        Root node of tree.
+
+    Returns
+    -------
+    A hash table comprising of unique key(sums)-value(counts) pairs.
+
+    Time Complexity
+    ---------------
+    O(N), where N is number of nodes in the tree. We use DFT here.
+
+    Space Complexity
+    ----------------
+    If we count hashtable in space complexity, we can sum((N/2^k)*(log(N) - (k-1)),
+    where k goes from 1 to log(N) and N is number of nodes. If we assume hastable as
+    part of output, then space complexity is maximum depth of recursion DFT stack,
+    which is log(N).
+    """
+    return depth_first_traversal(root_node, [], {})
 
 
 test = tree(3)
