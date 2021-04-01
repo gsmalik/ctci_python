d2b5cb87f19c6dcb5cf7489fb1b09a6039a961a2
Gurshaant Malik <gary.malik@appliedbrainresearch.com>
2021-03-31 21:47:48 -0400

    Add 9.8.5.py

--- chapter8/9.8.5.py
+++ chapter8/9.8.5.py
@@ -1,23 +1,41 @@
-def sort_nums(num1, num2):
-    # retuns bigger, smaller
-    if num1 > num2:
-        return num1, num2
-    else:
-        return num2, num1
+def mult_numbers(num1, num2):
+    """
+    Function to multiple 2 positive integers using only addition, subtraction and
+    bit shift operators.
 
+    Parameters
+    ----------
+    num1: int
+        First operand.
+    num2: int
+        Second operand.
 
-def mult_numbers(num1, num2, rec=0):
-    bigger_num, smaller_num = sort_nums(num1, num2)
+    Time Complexity
+    ---------------
+    O(log(N)), where N is the smaller of the 2 numbers.
 
+    Space Complexity
+    ----------------
+    O(log(N)), where N is the smaller of the 2 numbers.
+    """
+    # determine the bigger and smaller number.
+    bigger_num, smaller_num = (
+        num1 if num1 > num2 else num2,
+        num1 if num1 < num2 else num2,
+    )
+
+    # exit conditions
     if smaller_num == 1:
         return bigger_num
-    elif smaller_num == 0:
+    if smaller_num == 0:
         return smaller_num
-    else:
-        return mult_numbers(bigger_num, smaller_num >> 1, rec + 1) + mult_numbers(
-            bigger_num, smaller_num - (smaller_num >> 1), rec + 1
+    
+    # shift right smaller number and multiply it with bigger number + multiply
+    # remaining of the smaller number with bigger number.
+    return mult_numbers(bigger_num, smaller_num >> 1) + mult_numbers(
+            bigger_num, smaller_num - (smaller_num >> 1)
         )
 
 
-print(mult_numbers(17, 23))
-
+print(mult_numbers(4, 23))
+print(mult_numbers(4, 100))
