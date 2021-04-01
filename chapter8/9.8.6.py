358bda95a85d8de6681403c707f143ceea760465
Gurshaant Malik <gary.malik@appliedbrainresearch.com>
2021-03-31 22:35:13 -0400

    Add 9.8.6.py

--- chapter8/9.8.6.py
+++ chapter8/9.8.6.py
@@ -1,8 +1,39 @@
 def move_disks(num_disks, origin, buffer, destination):
+    """
+    Function to implement towers of hanoi problem.
+
+    Parameters
+    ----------
+    num_disks: int
+        Number of disks to move, starting from top.
+    origin: int
+        Index of rod to use as origin.
+    buffer: int
+        Index of rod to use as buffer while moving disks.
+    origin: int
+        Index of rod to move disks to.
+
+    Time Complexity
+    ---------------
+    O(2^N), where N is the number of disks.
+
+    Space Complexity
+    ----------------
+    O(N), where N is the number of disks.
+    """
+    # exit condition
     if num_disks <= 0:
         return
-    move_disks(num_disks-1, origin, destination, buffer)
+    # move top 'num-1' disks from origin to buffer using desintation as buffer.
+    test.append(0)
+    move_disks(
+        num_disks=num_disks - 1, origin=origin, buffer=destination, destination=buffer
+    )
     print("Moved disk #", num_disks, "from", origin, "to", destination)
-    move_disks(num_disks-1, buffer, origin, destination)
+    # move top 'num-1' disks from buffer to destination using origin as buffer.
+    move_disks(
+        num_disks=num_disks - 1, origin=buffer, buffer=origin, destination=destination
+    )
 
-move_disks(5,0,1,2)
+
+move_disks(5, 0, 1, 2)
