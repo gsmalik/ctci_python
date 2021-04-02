ce3b84feb50a72199317889cc6b395fa835bfeb3
Gurshaant Malik <gary.malik@appliedbrainresearch.com>
2021-04-02 16:21:47 -0400

    Add 9.8.8.py

--- chapter8/9.8.8.py
+++ chapter8/9.8.8.py
@@ -1,24 +1,49 @@
-def permute_string(list_char):
-    if len(list_char)==1:
-        return list_char
-    return create_list(list_char[0], permute_string(list_char[1:]))
-
-def create_list(character, permutations):
-    new_list = []
-    for comb in permutations:
-        for index in range(len(comb)+1):
-            new_list.append(comb[:index]+character+comb[index:])
-    return new_list
-
-def remove_duplicates(string_chars):
-    new_list = []
-    for character in string_chars:
-        if character not in new_list:
-            new_list.append(character)
-    return new_list
+# Create empty list to store permutations
+list_permutations = []
 
 
-test_string = ['a','b','c','d','e', 'e', 'a', 'b']
+def permute_string(current_string, avail_chars):
+    """
+    Function to create unique permutations by appending permutations of characters to
+    a string.
 
-print(permute_string(remove_duplicates(test_string)))
+    Parameters
+    ----------
+    current_string: str
+        Current string to which new permutations will be appended to.
+    avail_chars: list
+        List of characters out of which new permutations can be created.
 
+    Time Complexity
+    ---------------
+    O(N!), where N is the number of available characters.
+
+    Space Complexity
+    ----------------
+    O(N^2), where N is the number of available characters.
+    """
+    # If there are no available characters, then no new permutions can be created
+    # and you can append this current string to the list of permutations.
+    if not avail_chars:
+        list_permutations.append(current_string)
+        return
+
+    # Iterate through avail_chars and create a dictionary to track if a character
+    # can been used. True means its available to use and False means not.
+    dict_avail = {}
+    for character in avail_chars:
+        dict_avail[character] = True
+
+    # Create new permutations by iterating through characters and appending them
+    # to current string and creating permutations through remaining characters.
+    for index, character in enumerate(avail_chars):
+        if dict_avail[character]:
+            dict_avail[character] = False
+            permute_string(
+                current_string + character,
+                avail_chars[:index] + avail_chars[index + 1 :],
+            )
+
+
+permute_string("", ["a", "a", "b", "b", "c"])
+print(list_permutations)
