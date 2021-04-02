97668bc999c2c696304c1172906d161c03cc9227
Gurshaant Malik <gary.malik@appliedbrainresearch.com>
2021-04-02 16:09:21 -0400

    Add 9.8.7.py

--- chapter8/9.8.7.py
+++ chapter8/9.8.7.py
@@ -1,14 +1,44 @@
-def permute_string(list_char):
-    if len(list_char)==1:
-        return list_char
-    return create_list(list_char[0], permute_string(list_char[1:]))
+# Create empty list to store permutations
+list_permutations = []
 
-def create_list(character, permutations):
-    new_list = []
-    for comb in permutations:
-        for index in range(len(comb)+1):
-            new_list.append(comb[:index]+character+comb[index:])
-    return new_list
 
-print(permute_string(['a','b','c','d','e']))
+def permute_string(current_string, avail_chars):
+    """
+    Function to create unique permutations by appending permutations of characters to
+    a string.
 
+    Parameters
+    ----------
+    current_string: str
+        Current string to which new permutations will be appended to.
+    avail_chars: list
+        List of characters out of which new permutations can be created.
+
+    Time Complexity
+    ---------------
+    O(N!), where N is the number of available characters. You can count the number
+    of times 'permute_string' is called by creating a global empty test list and
+    appending a 0 to it every time at the start of this function call. You can then
+    divide the len of this list by the factorial of number of characters and see
+    that it will be a constant as you keep increasing the number of characters.
+
+    Space Complexity
+    ----------------
+    O(N), where N is the number of available characters.
+    """
+    # If there are no available characters, then no new permutions can be created
+    # and you can append this current string to the list of permutations.
+    if not avail_chars:
+        list_permutations.append(current_string)
+        return
+
+    # Create new permutations by iterating through characters and appending them
+    # to current string and creating permutations through remaining characters.
+    for index, character in enumerate(avail_chars):
+        permute_string(
+            current_string + character, avail_chars[:index] + avail_chars[index + 1 :]
+        )
+
+
+permute_string("", ["a", "b", "c", "d"])
+print(list_permutations)
