f9208a8ba8921846d25ecb1d0788de264854d8a3
Gurshaant Malik <gary.malik@appliedbrainresearch.com>
2021-04-07 18:44:29 -0400

    Add 9.8.14.py

--- chapter8/9.8.14.py
+++ chapter8/9.8.14.py
@@ -1,101 +1,70 @@
-def find_parenthesis(level, expression, result):
+def find_parenthesis_combinations(expression, expected_bool):
+    """
+    Function to calculate different parenthesis combinations to evaluate expression to
+    the expected boolean value.
 
+    Parameters
+    ----------
+    expression: str
+        Expression to find combinations for.
+
+    expected_bool: bool
+        Expected boolean value the expression needs to evaluate to.
+
+    Time Complexity
+    ---------------
+    Too complicated I think. But should be 4^N roughly speaking, where N is the number
+    of binary operators in the expression. The 4 comes from calculating left_false,
+    left_true, right_false and right_true. But the tree will not be balanced. So this
+    is a loose upper bound.
+
+    Space Complexity
+    ----------------
+    O(N), where N is the number of binary operators in the expression
+    """
+    # exit condition
     if len(expression) == 1:
-        return int(result == bool(int(expression[0])))
+        return int(expected_bool == bool(int(expression[0])))
 
-    ways = 0
+    # initialise number of ways for expression to evaluate to the expected bool
+    num_ways = 0
+
+    # iterate through operator symbols
     for index, symbol in enumerate(expression):
-        if result == True:
-            if symbol == "^":
-                ways = (
-                    ways
-                    + (
-                        find_parenthesis(level + 1, expression[:index], True)  # True
-                        * find_parenthesis(
-                            level + 1, expression[index + 1 :], False
-                        )  # False
-                    )
-                    + (
-                        find_parenthesis(level + 1, expression[:index], False)  # False
-                        * find_parenthesis(
-                            level + 1, expression[index + 1 :], True
-                        )  # True
-                    )
-                )
-            if symbol == "|":
-                ways = (
-                    ways
-                    + (
-                        find_parenthesis(level + 1, expression[:index], True)  # True
-                        * find_parenthesis(
-                            level + 1, expression[index + 1 :], False
-                        )  # False
-                    )
-                    + (
-                        find_parenthesis(level + 1, expression[:index], False)  # False
-                        * find_parenthesis(
-                            level + 1, expression[index + 1 :], True
-                        )  # True
-                    )
-                    + (
-                        find_parenthesis(level + 1, expression[:index], True)  # True
-                        * find_parenthesis(
-                            level + 1, expression[index + 1 :], True
-                        )  # True
-                    )
-                )
-            if symbol == "&":
-                ways = ways + (
-                    find_parenthesis(level + 1, expression[:index], True)  # True
-                    * find_parenthesis(level + 1, expression[index + 1 :], True)  # True
-                )
-        else:
-            if symbol == "^":
-                ways = (
-                    ways
-                    + (
-                        find_parenthesis(level + 1, expression[:index], True)  # True
-                        * find_parenthesis(
-                            level + 1, expression[index + 1 :], True
-                        )  # True
-                    )
-                    + (
-                        find_parenthesis(level + 1, expression[:index], False)  # False
-                        * find_parenthesis(
-                            level + 1, expression[index + 1 :], False
-                        )  # False
-                    )
-                )
-            if symbol == "|":
-                ways = ways + (
-                    find_parenthesis(level + 1, expression[:index], False)  # False
-                    * find_parenthesis(
-                        level + 1, expression[index + 1 :], False
-                    )  # False
-                )
-            if symbol == "&":
-                ways = (
-                    ways
-                    + (
-                        find_parenthesis(level + 1, expression[:index], True)  # True
-                        * find_parenthesis(
-                            level + 1, expression[index + 1 :], False
-                        )  # False
-                    )
-                    + (
-                        find_parenthesis(level + 1, expression[:index], False)  # False
-                        * find_parenthesis(
-                            level + 1, expression[index + 1 :], True
-                        )  # True
-                    )
-                    + (
-                        find_parenthesis(level + 1, expression[:index], False)  # False
-                        * find_parenthesis(
-                            level + 1, expression[index + 1 :], False
-                        )  # False
-                    )
-                )
-    return ways
+        # find all possible ways of evaluating left side and right side to True and False
+        if symbol in ["^", "|", "&"]:
+            left_false = find_parenthesis_combinations(expression[:index], False)
+            left_true = find_parenthesis_combinations(expression[:index], True)
+            right_false = find_parenthesis_combinations(expression[index + 1 :], False)
+            right_true = find_parenthesis_combinations(expression[index + 1 :], True)
+
+        # find number of ways for each symbol
+        if symbol == "^":
+            num_ways = num_ways + (
+                (left_false * right_true) + (left_true * right_false)
+                if expected_bool
+                else ((left_true * right_true) + (left_false * right_false))
+            )
+
+        if symbol == "|":
+            num_ways = num_ways + (
+                left_true * right_false
+                + left_true * right_true
+                + left_false * right_true
+                if expected_bool
+                else left_false * right_false
+            )
+
+        if symbol == "&":
+            num_ways = num_ways + (
+                left_true * right_true
+                if expected_bool
+                else left_false * right_false
+                + left_false * right_true
+                + left_true * right_false
+            )
+
+    return num_ways
 
 
 def create_expression_list(string):
@@ -108,4 +77,4 @@ def create_expression_list(string):
 test_string = "0&0&0&1^1|0"
 test_expression = create_expression_list(test_string)
 print(test_expression)
-print(find_parenthesis(0, test_expression, True))
+print(find_parenthesis_combinations(test_expression, True))
