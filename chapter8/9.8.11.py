553248a12dcebcc3c4a9ba739c5bfc4bdaf43aa5
Gurshaant Malik <gary.malik@appliedbrainresearch.com>
2021-04-07 20:35:56 -0400

    Add comments

--- chapter8/9.8.11.py
+++ chapter8/9.8.11.py
@@ -1,7 +1,38 @@
 def make_change(amount, list_denomination):
+    """
+    Function to count different ways of making change of given amount with given
+    denomination.
+
+    Parameters
+    ----------
+    amount: int
+        The amount of change.
+    list_denomination: list
+        The list of denomination to use to make change.
+    
+    Time Complexity
+    ---------------
+    O(C^N), where C is the amount and N is the number of different denominations. Note
+    that this is a very loose upper bound. We basically first think, for example in
+    case of [1,5,10,25] for making 100, that we can have 100 branches for 1, then
+    for each branch, we will have [5,10,25] and we can have 100 branches for 5 and then
+    100 branches for 10 and then 100 for 25, with an upper bound of 100^4. This is 
+    very loose.
+
+    Space Complexity
+    ----------------
+    O(N), where N is the number of different denominations.
+    """
+    # exit condition if amount is less than 0 or list of denomination is empty.
     if amount <= 0 or not list_denomination:
         return int(amount == 0)
+
+    # initialise number of ways
     ways = 0
+
+    # keep increasing count of first denomination until the left amount becomes
+    # negative. calculate ways of calculating said left amount with remaining
+    # denominations in list.
     num = 0
     while num * list_denomination[0] <= amount:
         ways = ways + make_change(
