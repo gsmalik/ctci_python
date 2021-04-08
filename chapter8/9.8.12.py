bd6fa2a0389339d5c31185f77db1e3e2e855ac2c
Gurshaant Malik <gary.malik@appliedbrainresearch.com>
2021-04-07 21:14:30 -0400

    Add comments

--- chapter8/9.8.12.py
+++ chapter8/9.8.12.py
@@ -3,19 +3,40 @@ import numpy as np
 list_ways = []
 
 
-def place_queen(list_previous_queens, n):
+def place_queen(list_previous_queens, N):
+    """
+    Function to place n queens such that no 2 queens lie along a diagonal, share a
+    column or row.
+
+    Parameters
+    ----------
+    list_previous_queens: list
+        List of placed queens, with each index correpsonding to the indexed numbered
+        column.
+    N: int
+        Size of chess board, where board dimension is N x N.
+
+    Time complexity
+    ---------------
+    O(N^3), where N is the size of the chessboard.
+
+    Space Complexity
+    ----------------
+    O(N), where N is the size of the chessboard.
+
+    """
     # check if already placed queens are failing constraints
     if failed_constraints(list_previous_queens):
         return
     # if all queens are place and we have not violated constraints, this means
     # we have a valid placement. add it to list of valid placements.
-    if len(list_previous_queens) == n:
+    if len(list_previous_queens) == N:
         list_ways.append(list_previous_queens)
         return
 
     # try each row for this queen
-    for row in range(n):
-        place_queen(list_previous_queens + [row], n)
+    for row in range(N):
+        place_queen(list_previous_queens + [row], N)
 
 
 def failed_constraints(list_placement):
