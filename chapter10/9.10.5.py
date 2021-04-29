8e52bb5a5ac96e00a6f60c691092a8dfa4ebcd07
Gurshaant Malik <gary.malik@appliedbrainresearch.com>
2021-04-29 19:58:12 -0400

    Add 9.10.5.py

--- chapter10/9.10.5.py
+++ chapter10/9.10.5.py
@@ -1,43 +1,67 @@
 def find_string(array, string, left, right):
-    # Return if left most element matches
-    if array[left] == string:
-        return left
-    # Return if right most element matches
-    if array[right] == string:
-        return right
-    # Return if length is 1 since no match
-    if left == right:
+    """
+    Function to search for a string in a sorted list of strings, interspersed with
+    empty strings
+
+    Parameters
+    ----------
+    array: list
+        List of strings.
+    string: str
+        String that we are searching for.
+    left: int
+        Left index of sub array to be searched.
+    right: int
+        Right index of sub array to be searched.
+
+    Time Complexity
+    ---------------
+    O(N) worst case because we have to traverse array to find non empty strings when
+    binary search leads results in probing an empty string. This is still better than
+    linear traversing on average, since binary search can cut down non empty string
+    probes to logN.
+
+    Space Complexity
+    ----------------
+    O(logN) worst case when every time we hit a non empty string doing binary search and
+    hence have a recursive depth of logN.
+    """
+    # find mid point
+    mid = (left + right) // 2
+
+    # if only element left to be searched
+    if left == right and array[left] != string:
         return False
 
-    # Calculate mid point
-    mid = left + (right - left) // 2
-
-    # Return if mid point matches
-    if array[mid] == string:
-        return mid
-
-    # If mid point is empty string
+    # if mid point is empty string
     if array[mid] == "":
 
-        # Keep searching for left/right most non empty string
-        mid_right = mid + 1
-        mid_left = mid - 1
-        while mid_left > 0 and array[mid_left] == "":
+        mid_left = mid_right = mid
+        # search for first non empty string going left
+        while mid_left >= left and array[mid_left] == "":
             mid_left -= 1
-        while mid_right < len(array) - 1 and array[mid_right] == "":
+        # search for first non empty string going right
+        while mid_right <= right and array[mid_right] == "":
             mid_right += 1
 
-        # Update mid point if string lies left or right
-        if array[mid_left] != "" and array[mid_left] >= string:
-            mid = mid_left
-        elif array[mid_right] != "" and array[mid_right] <= string:
-            mid = mid_right
-        # There will be no match. Return false
-        else:
+        # if search exhausted, return false
+        if mid_left < left and mid_right > right:
             return False
 
-    # Recurse into appropriate direction
-    if array[mid] >= string:
+        # if left is non empty and right was exhausted, choose left
+        if array[mid_left] != "" and mid_right > right:
+            mid = mid_left
+        # if right is non empty and left was exhausted, choose right
+        elif array[mid_right] != "" and mid_left < left:
+            mid = mid_right
+        # if both become non empty, choose nearest one to mid
+        else:
+            mid = mid_left if abs(mid - mid_left) < abs(mid - mid_right) else mid_right
+
+    if array[mid] == string:
+        return mid
+    # recurse into the right direction
+    if array[mid] > string:
         return find_string(array, string, left, mid)
     else:
         return find_string(array, string, mid, right)
