de330017135a8f81cdcbaf67c34d663dcd71953d
Gurshaant Malik <gary.malik@appliedbrainresearch.com>
2021-05-01 14:05:58 -0400

    add 9.10.11.py

--- chapter10/9.10.11.py
+++ chapter10/9.10.11.py
@@ -2,21 +2,32 @@ import numpy as np
 
 
 def create_peaks_valleys(array):
-    # create valleys
-    for index in range(1, len(array) - 1, 2):
-        min_elem = min(array[index - 1], array[index], array[index + 1])
-        if min_elem == array[index - 1]:
-            array[index], array[index - 1] = array[index - 1], array[index]
-        elif min_elem == array[index + 1]:
+    """
+    Function to arrange given array into alternating peaks and valleys.
+
+    Parameters
+    ----------
+    arrays: np.1darray
+        Array that needs to be arranged
+
+    Time Complexity
+    ---------------
+    O(N), where N is the number of elements in the array.
+
+    Space Complexity
+    ----------------
+    O(1).
+    """
+    for index in range(len(array) - 1):
+        peak = index % 2 == 0
+        valley = not peak
+        if (peak and array[index] < array[index + 1]) or (
+            valley and array[index] > array[index + 1]
+        ):
             array[index], array[index + 1] = array[index + 1], array[index]
-
-    # Create last peak
-    if len(array) % 2 == 0:
-        if array[-1] > array[-2]:
-            array[-1], array[-2] = array[-2], array[-1]
-
     return array
 
 
-test = np.random.randint(0, 10, 11)
+test = np.random.randint(0, 100, 10)
+print(test)
 print(create_peaks_valleys(test))
